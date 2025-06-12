32cf0831735f415a869459885d6368922626edd8
Syariqulhusni26 <syariqulhusni26@gmail.com>
2025-06-12 14:58:21 +0700

    upload folder cmake

---
+++ cmake-4.0.0-rc1/Tests/RunCMake/cmake_minimum_required/RunCMakeTest.cmake
@@ -0,0 +1,11 @@
+include(RunCMake)
+
+run_cmake(BeforeVersionRemoved)
+run_cmake(PolicyBeforeVersionRemoved)
+run_cmake(Future)
+run_cmake(BeforeVersionDeprecated)
+run_cmake(Range)
+run_cmake(RangeBad)
+run_cmake(Unknown)
+run_cmake(PolicyVersionVariable)
+run_cmake(PolicyVersionVariableBad)
